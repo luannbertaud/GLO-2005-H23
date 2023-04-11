03b9f90b7f346e7149b331b6e81ed8c4d2b572f7
Luann <luann.bertaud@epitech.eu>
2023-04-10 21:11:35 -0400

    frontend : feature : Token handling architecture + profile & disconnect menu

--- sources/frontend/app/page.tsx
+++ sources/frontend/app/page.tsx
@@ -1,13 +1,28 @@
+"use client";
+
 import CitationCard from "@/components/CitationCard";
+import {useEffect, useState} from 'react';
+import { ValidateAccess } from "@/components/Access";
+import {useRouter} from "next/navigation";
+import {useCookies} from "react-cookie";
+import Loading from "@/app/loading";
 
 export default function Feed() {
-  return (
-    <div className="grid grid-row-1 gap-12 w-screen h-screen max-h-screen overflow-y-scroll items-center justify-center pt-5">
-      <CitationCard/>
-      <CitationCard/>
-      <CitationCard/>
-      <CitationCard/>
-      <CitationCard/>
-    </div>
-  )
+    const router = useRouter();
+    const [cookies]: [any, any, any] = useCookies(['user']);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        ValidateAccess(router, cookies["ipaper_user_token"]).then(() => setLoading(false));
+    })
+    if (loading) return <Loading/>;
+    return (
+        <div className="grid grid-row-1 gap-12 w-screen h-screen max-h-screen overflow-y-scroll items-center justify-center pt-5">
+            <CitationCard/>
+            <CitationCard/>
+            <CitationCard/>
+            <CitationCard/>
+            <CitationCard/>
+        </div>
+    )
 }
