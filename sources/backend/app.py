5996cc4c182b429e068d6bd62a406b9bb244e321
Luann <luann.bertaud@epitech.eu>
2023-04-17 12:01:40 -0400

    Documentation de l'API

--- sources/backend/app.py
+++ sources/backend/app.py
@@ -20,12 +20,14 @@ app = Flask(__name__)
 CORS(app)
 asgi_app = WsgiToAsgi(app)
 
+# Declaration des dépots pour chaques relations.
 users_repository = UsersRepository()
 likes_repository = LikesRepository(users_repository)
 posts_repository = PostsRepository(users_repository)
 comments_repository = CommentsRepository(users_repository)
 notif_repository = NotificationsRepository(users_repository)
 
+# Nous utilisons des services pour gérer quelque logique pour les objets des relations.
 users_service = UsersService(users_repository, likes_repository)
 likes_service = LikesService(users_repository, likes_repository)
 posts_service = PostsService(users_repository, posts_repository, comments_repository, likes_repository)
@@ -36,6 +38,10 @@ notif_service = NotificationsService(users_repository, notif_repository)
 
 @app.route('/')
 def heartbeat():
+    """
+    Endpoint qui permet de savoir si l'API est bien fonctionnelle.
+    :return: Toujours un message de succès et un code 200.
+    """
     return 'Welcome to InstaPaper API', 200
 
 
@@ -44,24 +50,44 @@ def heartbeat():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    Endpoint qui permet l'obtention d'un token d'authentification valide en échange d'un
+    payload contenant un champ 'email' et un champ 'password'.
+    :return: Le token d'authentification ou une InvalidParameterException.
+    """
     token = auth_service.login(request.get_json())
     return json.dumps(token), 200
 
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    """
+    Endpoint qui permet la création d'un compte utilisateur et l'obtention d'un token
+    d'authentification valide en échange d'un payload contenant les champs
+    'email', 'password' 'username', 'lastname' et 'bio'.
+    :return: Le token d'authentification ou une InvalidParameterException.
+    """
     token = users_service.create_user(request.get_json())
     return json.dumps(token), 201
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
+    """
+    Endpoint qui supprime le token enregistré pour un certain utilisateur, ce qui confirme l'impossibilité
+    de se connecter avec ce token.
+    :return: Un message de succès et un code 200.
+    """
     auth_service.logout(request.headers.get("X-token-id"))
     return 'Logout successful', 200
 
 
 @app.route('/verify_token', methods=['GET'])
 def verify_token():
+    """
+    Endpoint qui permet la vérification de la validité d'un token.
+    :return: Un code 200 ou 401 suivant la validité du token.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     else:
@@ -72,6 +98,13 @@ def verify_token():
 
 @app.route('/profile/<string:username>', methods=['GET', 'DELETE'])
 def get_user_profil(username):
+    """
+    Endpoint qui permet d'obtenir les informations sur un utilisateur en particulier.
+    L'utilisateur doit être connecté pour accéder à cet endpoint.
+    :param username: Le nom d'utilisateur de du compte sur lequel nous souhaitons obtenir des informations.
+    :return: Un json contenant les informations suivantes : 'username', 'email', 'firstname', 'lastname', 'bio'
+    'following', 'followers', 'likes'.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     if request.method == 'GET':
@@ -85,6 +118,11 @@ def get_user_profil(username):
 
 @app.route('/like', methods=['POST', 'DELETE'])
 def like():
+    """
+    Endpoint qui permet à un utilisateur de liker un post, ou de supprimer son like.
+    :return: Un code 200 en cas de réussite, 401 si l'utilisateur
+    n'est pas autorisé à faire cette action sur le Post demandé.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     if request.method == 'POST':
@@ -97,6 +135,11 @@ def like():
 
 @app.route('/post', methods=['POST'])
 def create_post():
+    """
+    Endpoint qui permet à un utilisateur de créer un Post en son nom.
+    Les champs requis sont 'author', 'body' et 'police'.
+    :return: Un code 200 de succès ou une InvalidParameterException.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     return posts_service.post(request.headers.get("X-token-id"), request.get_json())
@@ -104,6 +147,11 @@ def create_post():
 
 @app.route('/post/<string:post_id>', methods=['DELETE'])
 def delete_post(post_id: str):
+    """
+    Endpoint qui permet à un utilisateur de supprimer un Post lui appartenant.
+    :param post_id: L'id du post à supprimer.
+    :return: Un code 200 de succès ou un code 400 si l'utilisateur n'est pas autorisé à supprimer ce Post.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     return posts_service.delete_post(request.headers.get("X-token-id"), post_id)
@@ -111,6 +159,12 @@ def delete_post(post_id: str):
 
 @app.route('/search/<string:query>', methods=['GET'])
 def search_user(query: str):
+    """
+    Endpoint qui permet de recherche un utilisateur dont le nom d'utilisateur
+    contient une certaine chaine de caractères.
+    :param query: Chaine de caractères devant être contenue dans le nom d'utilisateur.
+    :return: Un code 200 de succès et une liste des utilisateurs correspondant à la recherche.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     else:
@@ -123,6 +177,11 @@ def search_user(query: str):
 
 @app.route('/notifs', methods=['GET', 'PATCH'])
 def get_last_notifs():
+    """
+    Endpoint qui permet à un utilisateur de récolter les notifications qui le concerne.
+    Cette méthode permet aussi de changer le status d'une notification de 'unread' à 'read'.
+    :return: Un code 200 avec un message de succès ou une InvalidParameterException.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     if request.method == 'GET':
@@ -141,6 +200,13 @@ def get_last_notifs():
 
 @app.route('/posts', methods=['GET'])
 def latest_posts():
+    """
+    Endpoint qui permet à un utilisateur récolter les derniers posts postés sur
+    la plateforme de façon chronologique inverse.
+    Deux paramètres peuvent être passés dans l'url de manière à gérer la pagination.
+    Ex. GET .../posts?page=2&page_size=10
+    :return: Une list des Posts contenu dans la page demandée.
+    """
     page = request.args.get("page") or 0
     page_size = request.args.get("page_size") or 10
     page = int(page)
@@ -153,6 +219,11 @@ def latest_posts():
 
 @app.route('/posts/<string:username>', methods=['GET'])
 def user_posts(username: str):
+    """
+    Endpoint qui permet à un utilisateur récolter les derniers posts postés par un utilisateur specific.
+    :param username: Le nom d'utilisateur à qui doivent appartenir les Posts.
+    :return: Une list des Posts appartenant à l'utilisateur.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     res = posts_service.get_posts_for_user(request.headers.get("X-token-id"), username)
@@ -164,6 +235,12 @@ def user_posts(username: str):
 
 @app.route('/comments/<int:comment_id>', methods=['GET'])
 def get_comment(comment_id: int):
+    """
+    Endpoint qui permet d'obtenir les informations d'un commentaire specific.
+    Les données retournées sont : 'post_id', 'author', 'body', 'timestamp'.
+    :param comment_id: L'id du commentaire ciblé.
+    :return: Les données correspondantes au commentaire ciblé.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     return json.dumps(comments_service.get_comment_by_id(comment_id)), 200
@@ -171,6 +248,11 @@ def get_comment(comment_id: int):
 
 @app.route('/comments/<int:comment_id>', methods=['DELETE'])
 def delete_comment(comment_id: int):
+    """
+    Endpoint permettant à un utilisateur de supprimé un commentaire lui appartenant.
+    :param comment_id: L'id du commentaire à supprimer.
+    :return: Code 200 en cas de succès, code 401 su l'utilisateur n'est pas autorisé à executer cette action.
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     return comments_service.delete(request.headers.get("X-token-id"), comment_id)
@@ -178,6 +260,11 @@ def delete_comment(comment_id: int):
 
 @app.route('/comments', methods=['POST'])
 def create_comment():
+    """
+    Endpoint qui permet à un utilisateur de créer un commentaire. Les données nécessaires sont :
+    'post_id' et 'body'.
+    :return: Les données du Post juste créé ou une InvalidParameterException
+    """
     if auth_service.is_token_valid(request.headers.get("X-token-id")) is False:
         return 'Invalid token', 401
     return comments_service.create(request.headers.get("X-token-id"), request.get_json()), 200
