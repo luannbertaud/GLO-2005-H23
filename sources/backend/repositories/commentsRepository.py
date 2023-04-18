972874d197b64d100eeb26cb673071cbc45a53ed
Luann <luann.bertaud@epitech.eu>
2023-04-18 09:54:48 -0400

    Documentation for CommentsRepository

--- sources/backend/repositories/commentsRepository.py
+++ sources/backend/repositories/commentsRepository.py
@@ -19,6 +19,11 @@ class CommentsRepository:
         )
 
     def get_comments_of_post(self, post_id: int):
+        """
+        Méthode permettant d'obtenir tous les commentaires d'un post en fournissant son id.
+        :param post_id: L'id du post auquel sont reliés les commentaires.
+        :return: Une liste des commentaires du post.
+        """
         connection = self.__create_connection()
         try:
             cursor = connection.cursor()
@@ -31,6 +36,11 @@ class CommentsRepository:
         return comments
 
     def delete_comment(self, comment_id: int):
+        """
+        Supprime un commentaire dans la base de données.
+        :param comment_id: L'id avec lequel identifier le commentaire à supprimer.
+        :return: True si le commentaire a été supprimé, False sinon.
+        """
         connection = self.__create_connection()
         try:
             cursor = connection.cursor()
@@ -41,6 +51,11 @@ class CommentsRepository:
         return affected_columns != 0
 
     def get_comment_by_id(self, comment_id):
+        """
+        Méthode permettant d'obtenir l'objet complet d'un commentaire à partir de son id.
+        :param comment_id: Id avec lequel identifier le commentaire à charger depuis la base de données.
+        :return: Un Json contenant toutes les informations disponibles sur le commentaire.
+        """
         connection = self.__create_connection()
         comment = None
         try:
@@ -56,6 +71,12 @@ class CommentsRepository:
         return comment
 
     def create_comment(self, post_id, author, body, timestamp):
+        """
+        Méthode permettant de créer un commentaire dans la base de données suivant les valeurs passées en
+        paramètre. Toutes les valeurs en paramètre sont nécessaires.
+        :return: Un tuple contenant : un boolean indiquant si le commentaire a été créé, et l'id du commentaire créé
+        (-1 en cas d'échec).
+        """
         connection = self.__create_connection()
         affected_columns = 0
         try:
